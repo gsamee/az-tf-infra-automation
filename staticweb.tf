fec6ad5edb9948f8536d59fb69b2db1d5fc8b112
Sameera Punchihewa <Sameera.Punchihewa@medfarsolutions.com>
2023-10-27 17:42:42 -0400

    add ip config

--- staticweb.tf
+++ staticweb.tf
@@ -14,3 +14,18 @@ resource "azurerm_static_site" "static_site" {
 }
 
 
+resource "azurerm_public_ip" "sam_pip" {
+    allocation_method       = "Static"
+    ddos_protection_mode    = "VirtualNetworkInherited"
+    idle_timeout_in_minutes = 4
+    ip_tags                 = {}
+    ip_version              = "IPv4"
+    location                = "canadacentral"
+    name                    = "pip-samm-test"
+    resource_group_name     = "rg-mfmobile3-dev-centralus"
+    sku                     = "Standard"
+    sku_tier                = "Regional"
+    tags                    = {
+        "CreatedBy" = "Sameera"
+    }
+}
